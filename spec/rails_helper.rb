69545441ddda2ad56fca7cf56f97c7ade742bd6a
Kristina Lim <kristinalim.ph@gmail.com>
2018-07-19 18:09:06 +0800

    Install and set up factory_bot gem

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -71,4 +71,9 @@ RSpec.configure do |config|
   # config.filter_gems_from_backtrace("gem name")
 
   config.include ActiveSupport::Testing::TimeHelpers
+  config.include FactoryBot::Syntax::Methods
+
+  config.before(:suite) do
+    FactoryBot.find_definitions
+  end
 end
