0d066ca592f1d5e66d59f1a2d7c8f181475d720b
Kristina Lim <kristinalim.ph@gmail.com>
2018-07-19 20:35:47 +0800

    Set default host and port for mail links in test

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -27,6 +27,7 @@ Rails.application.configure do
 
   # Disable request forgery protection in test environment.
   config.action_controller.allow_forgery_protection = false
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   config.action_mailer.perform_caching = false
 
   # Tell Action Mailer not to deliver emails to the real world.
