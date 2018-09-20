63bca24222dca68d3e08feb3418e87f4f45d4f3f
Sjors Provoost <sjors@sprovoost.nl>
2018-09-20 15:49:49 +0100

    Bump Bitcoin Core to v0.16.3

--- vendor/bitcoin/install.sh
+++ vendor/bitcoin/install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 cd /tmp
-export BITCOIN_VERSION=0.16.0
-export BITCOIN_URL=https://bitcoincore.org/bin/bitcoin-core-0.16.0/bitcoin-0.16.0-x86_64-linux-gnu.tar.gz
-export BITCOIN_SHA256=e6322c69bcc974a29e6a715e0ecb8799d2d21691d683eeb8fef65fc5f6a66477
-export BITCOIN_ASC_URL=https://bitcoincore.org/bin/bitcoin-core-0.16.0/SHA256SUMS.asc
+export BITCOIN_VERSION=0.16.3
+export BITCOIN_URL=https://bitcoincore.org/bin/bitcoin-core-0.16.3/bitcoin-0.16.3-x86_64-linux-gnu.tar.gz
+export BITCOIN_SHA256=5d422a9d544742bc0df12427383f9c2517433ce7b58cf672b9a9b17c2ef51e4f
+export BITCOIN_ASC_URL=https://bitcoincore.org/bin/bitcoin-core-0.16.3/SHA256SUMS.asc
 export BITCOIN_PGP_KEY=01EA5486DE18A882D4C2684590C8019E36C2E964
 wget -qO bitcoin.tar.gz "$BITCOIN_URL"
 echo "$BITCOIN_SHA256 bitcoin.tar.gz" | sha256sum -c - \
